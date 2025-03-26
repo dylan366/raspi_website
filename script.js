e36d0f104579135d3dbc875c85ad12b3e31fad31
dylan366 <dycetina@ksr.ch>
2025-03-26 17:31:37 +0100

    button

---
+++ script.js
@@ -0,0 +1,9 @@
+const closeGameButton = document.getElementById("close-game-window");
+const appWindow = document.getElementById("game-application-window");
+
+
+function closeWindow() {
+    console.log("Hello World!");
+}
+
+closeGameButton.addEventListener('click', closeWindow);
